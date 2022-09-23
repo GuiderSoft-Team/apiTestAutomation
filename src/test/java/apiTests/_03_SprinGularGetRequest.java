aa02791acfdb64d415fe156587ed75c75eca5d94
mehme <mehmet.olguntr@gmail.com>
2022-09-23 21:58:19 +0300

    Added Day_8 _03_SprinGularGetRequest.java

--- src/test/java/apiTests/_03_SprinGularGetRequest.java
+++ src/test/java/apiTests/_03_SprinGularGetRequest.java
@@ -12,14 +12,35 @@ public class _03_SprinGularGetRequest {
 
     String sprinGularUrl = "http://142.93.110.12:9119";
     String accessToken = "Bearer " +
-            "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NjM3MDU5MDUsInN1YiI6Ik1yaW5tb3lNYWp1bWRhciIsInVzZXJJZCI6Imd1aWRlcnNvZnQiLCJyb2xlIjoiVVNFUiJ9.igIfBoRWZlaoV_hXjg1VEqrKsaOj3jJaAlvLmL4WTis";
+            "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NjM5NTU0NjAsInN1YiI6Ik1yaW5tb3lNYWp1bWRhciIsInVzZXJJZCI6Imd1aWRlcnNvZnQiLCJyb2xlIjoiVVNFUiJ9.Pc7cPuQfXBv3mD8gHH5Kwvf8iLoyim71FN3TZWf2wTA";
 
     @Test
-    public void test1(){
-        Response response = given().header("Authorization",accessToken)
-                .when().get(sprinGularUrl+"/api/orders");
+    public void test1() {
+        Response response = given().header("Authorization", accessToken)
+                .when().get(sprinGularUrl + "/api/orders");
 
         response.prettyPrint();
-        Assert.assertEquals(response.statusCode(),200);
+        Assert.assertEquals(response.statusCode(), 200);
+    }
+
+    // orderID si 4003 olan order ı print ediniz.
+
+    @Test
+    public void test2() {
+
+        Response response = given().header("Authorization", accessToken)
+                .when().get(sprinGularUrl + "/api/orders?orderid=4003");
+
+        response.prettyPrint();
+    }
+
+    // orderID si 4500 olan order ı print ediniz.
+    //status code 200 olduğunu test ediniz.
+
+    @Test
+    public void test3(){
+        Response response= given().header("Authorization", accessToken)
+                .when().get(sprinGularUrl + "/api/orders?orderid=4500");
+        response.prettyPrint();
     }
 }
